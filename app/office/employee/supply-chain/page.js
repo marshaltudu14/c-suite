feabcf372b70e97c1771129346fb05b76afdf449
Gaurav <gs0023706@gmail.com>
2025-02-07 15:31:31 +0530

    aaa

--- app/office/employee/supply-chain/page.js
+++ app/office/employee/supply-chain/page.js
@@ -1,7 +1,8 @@
+import ChatInterface from "@/app/_components/ChatInterface";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return <ChatInterface />;
 };
 
 export default page;
