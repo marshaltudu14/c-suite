eb6bcf0e3b7cff9339ac933f28fdb9251ec67e54
Gaurav <gs0023706@gmail.com>
2025-02-08 12:34:29 +0530

    ddd

--- app/office/executive/ceo/page.js
+++ app/office/executive/ceo/page.js
@@ -2,10 +2,16 @@ import ChatInterface from "@/app/_components/ChatInterface";
 import React from "react";
 
 const CeoPage = () => {
+  const CeoData = {
+    name: "Satya",
+    position: "CEO",
+    image: "/CEO.webp",
+    systemPrompt: `You are the CEO of OfStartup. Provide business related guidance, planning, and respond with a professional visionary approach.`,
+  };
   return (
     <div>
       <div>
-        <ChatInterface isOnline={true} role="ceo" />
+        <ChatInterface personaData={CeoData} />
       </div>
     </div>
   );
